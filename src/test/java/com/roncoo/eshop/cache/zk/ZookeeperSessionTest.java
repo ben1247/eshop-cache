d46d7e3e719d01561334f45ac104b1845a950c4c
张 悦 <yue.zhang@shuyun.com>
2017-07-29 21:52:58 +0800

    使用zookeeper实现分布式锁

---
+++ src/test/java/com/roncoo/eshop/cache/zk/ZookeeperSessionTest.java
@@ -0,0 +1,18 @@
+package com.roncoo.eshop.cache.zk;
+
+/**
+ * Component:
+ * Description:
+ * Date: 17/7/27
+ *
+ * @author yue.zhang
+ */
+public class ZookeeperSessionTest {
+
+    public static void main(String [] args){
+        ZookeeperSession zookeeperSession = ZookeeperSession.getInstance();
+
+
+    }
+
+}
